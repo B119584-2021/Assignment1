8a0ef7f9e0d58e255a18b1493b44f1bed71f5863
B119584-2021 <smwinterbourne@gmail.com>
2021-11-01 10:11:02 +0000

    did some formatting and checked the file paths again

--- bash_scripts/quality_check.sh
+++ bash_scripts/quality_check.sh
@@ -1,35 +1,47 @@
 #!/bin/bash
+
 #This script performs a quality check on the raw sequences using the fastqc tool
 
 #Make a directory to hold the quality check reports
-mkdir quality_check
+mkdir fastqc_report
 
-#Change directories to where the input files are 
-cd input_seq
-
-#Run FASTQC
+#set variable containing .fq.gz files to be analysed
 FILES="/localdisk/home/data/BPSM/AY21/fastq/*.fq.gz"
 
-for FILE in $FILES
-do fastqc $FILE -o ../quality_check -q
-  echo "Fastqc analysis complete for $FILE"
+for FILE in $FILES ; do
+fastqc $FILE -o fastqc_report -q
+echo "Fastqc analysis complete for ${FILE: 37:}"
 done 
 
-# Make a directory to contain the html reports 
-mkdir html_reports
+# Make a directory to contain the html reports inside quality_check
+mkdir fastqc_report/html_reports
 
 # Move the html reports into the html_reports directory  
-mv *.html html_reports
+mv fastqc_report/*.html fastqc_report/html_reports
 
-#Return to home directory 
-cd ../
 
-ls -l input_seq/*.fq.gz | wc | awk -F ' ' ' {print ""$1" were analysed by fastqc analysis"}'
+ls -l /localdisk/home/data/BPSM/AY21/fastq/*.fq.gz | wc | awk -F ' ' ' {print ""$1" were analysed by fastqc analysis"}'
 
-ls -l quality_check/*.zip | wc | awk -F ' ' ' {print ""$1" zip files were generated from fastqc analysis"}'
+ls -l fastqc_report/*.zip | wc | awk -F ' ' ' {print ""$1" zip files were generated from fastqc analysis"}'
 
-ls -l quality_check/*.html | wc | awk -F ' ' ' {print ""$1" html files were generated from fastqc analysis"}'
+ls -l fastqc_report/*.html | wc | awk -F ' ' ' {print ""$1" html files were generated from fastqc analysis"}'
 
-#$ -o quality_check.o
-#$ -e quality_check.e
+
+
+#assign values to FILES
+SAMPLE="*.zip"
+
+#unzip the files in quality_check
+for sample in $SAMPLE ; do 
+  unzip -q $sample
+  echo "$sample has been unzipped"
+  #remove zipped files 
+  rm -f $sample
+done
+
+SUM="*fastqc/summary.txt"
+
+for sum in $SUM ; do
+cat $sum | grep "FAIL" >> fail_warnings.txt
+done
 
